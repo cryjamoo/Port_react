6d6b69b75b96a3c7cdf310e9787bf25912c392cb
cryjamoo <xboqqdidi@gmail.com>
2025-03-27 14:21:07 +0700

    final

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar'; // เพิ่มการนำเข้า Navbar
-
 import Home from './Home';
 import Profile from './Profile';
 import Skills from './Skills';
